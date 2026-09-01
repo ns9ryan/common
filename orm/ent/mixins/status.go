4eccfbbb8b1feca6fe6a4b291dc34f1b9c9846ef
ryan <ryan@company.com>
2026-09-01 14:41:07 +0800

    fix: 修改README.md

---
+++ orm/ent/mixins/status.go
@@ -0,0 +1,22 @@
+package mixins
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// StatusMixin 通用状态字段，适用于多状态场景
+type StatusMixin struct {
+	mixin.Schema
+}
+
+// Fields 定义通用状态字段
+func (StatusMixin) Fields() []ent.Field {
+	return []ent.Field{
+		field.Uint8("status").
+			Default(1).
+			Optional().
+			Comment("状态：1 正常，2 停用"),
+	}
+}
